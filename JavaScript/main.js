f02b93b66aaecfe28e573480f54faecb59350480
Ioan Georgiev <ioan.georgiev62@gmail.com>
2016-04-20 17:11:53 +0300

    This is my training repository

---
+++ JavaScript/main.js
@@ -0,0 +1,7 @@
+﻿//var content = document.getElementById("pedal");
+//content.onclick = function changeContent() {
+//    document.getElementById("demo1").innerHTML = "Hello MotherFucker!";
+//    document.getElementById("demo2").innerHTML = "I'm your fucking father!";
+//}
+
+var price1 = 
